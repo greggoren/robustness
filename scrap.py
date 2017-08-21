4a4601c664f04eab957f543d142daeee97bfba10
gregory goren <grggoren@gmail.com>
2017-08-21 18:34:43 +0300

    optimal document creation per model

--- scrap.py
+++ scrap.py
@@ -1,7 +1,8 @@
 import math
 import optimizer
+import models_handler
+import pickle
 if __name__=="__main__":
-    opt= optimizer.optimizer(0.001, [1,2,3])
-    x_0=[0.1,0,0.3]
-    res = opt.get_best_features(x_0)
-    print(res['x'])
+    with open("model_handler.pickle",'rb') as f:
+        a = pickle.load(f)
+        print(a.weights_index)
