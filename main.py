a0d16a3f339b5f05b87bffc695d32aedbc5295bf
gregory goren <grggoren@gmail.com>
2017-08-30 01:02:35 +0300

    entropy addition

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import random as r
 import params_ent
 import preprocess_clueweb as p
 from sklearn.model_selection import GroupKFold
+from sklearn.datasets import dump_svmlight_file
 import numpy as np
 if __name__=="__main__":
     preprocess = p.preprocess()
     X, y, queries = preprocess.retrieve_data_from_file("featuresCB_asr")
-    print(np.where(np.isnan(X)))
+    dump_svmlight_file(X,y,"nan")
 
 
