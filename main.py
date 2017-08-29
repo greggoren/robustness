31ac3b328eb055188a7eb7d9208ee63aa798263b
gregory goren <grggoren@gmail.com>
2017-08-30 00:58:20 +0300

    entropy addition

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random as r
 import params_ent
+import preprocess_clueweb as p
+from sklearn.model_selection import GroupKFold
+import numpy as np
 if __name__=="__main__":
-    for gamma in params_ent.gammas:
-        print(gamma)
-        params_ent.score_file = str(gamma)+"_test.txt"
-        
+    preprocess = p.preprocess()
+    X, y, queries = preprocess.retrieve_data_from_file("featuresCB_asr")
+    print(np.where(np.isnan(X.toarray())))
+
+
