fe4d9338907b46b7111d748ed0fbaf55f6f8b5d5
gregory goren <grggoren@gmail.com>
2017-08-30 09:13:00 +0300

    entropy addition

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.datasets import dump_svmlight_file
 import numpy as np
 if __name__=="__main__":
     preprocess = p.preprocess()
-    X, y, queries = preprocess.retrieve_data_from_file("featuresCB_asr",False)
+    X, y, queries = preprocess.retrieve_data_from_file("featuresCB_asr",True)
     print(X.max(axis=0))
     print(X.min(axis=0))
     # dump_svmlight_file(X,y,"nan",zero_based=False,query_id=queries)
