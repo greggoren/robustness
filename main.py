8c5c2521afc4378f6ccb503f740d2934e58ffac9
gregory goren <grggoren@gmail.com>
2017-08-30 09:22:39 +0300

    entropy addition

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ import numpy as np
 if __name__=="__main__":
     preprocess = p.preprocess()
     X, y, queries = preprocess.retrieve_data_from_file("features",True)
-    print(X.max(axis=0))
-    print(X.min(axis=0))
     # dump_svmlight_file(X,y,"nan",zero_based=False,query_id=queries)
+    xmx = X.max(axis=0)
+    xmn= X.min(axis=0)
+    for i in range (len(xmn)):
+        if xmx[i]<=xmn[i]:
+            print("BIG SHIT!!!!")
+
 
 
