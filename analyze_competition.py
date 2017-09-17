9c7c33c8cf3e0f58ecbe548770436b69fa0fc3be
gregory goren <grggoren@gmail.com>
2017-09-17 15:55:15 +0300

    initial weights addition

--- analyze_competition.py
+++ analyze_competition.py
@@ -293,26 +293,28 @@ class analysis:
 
 
 
-    def analyze(self,svms,competition_data):
+    def analyze(self,svms,competition_data,retrieval):
         scores = self.get_all_scores(svms,competition_data)
         rankings_svm = self.retrieve_ranking(scores)
         for svm in svms:
             if svm[2]=="svm_epsilon":
                 rankings_svm[svm],scores = self.rerank_by_epsilon(svm,scores,1.5)
-        # kendall, cr,rbo_min,x_axis = self.calculate_average_kendall_tau(rankings_svm)
-        # create_plot("Average Kendall-Tau with last iteration","plt/kt1_init.PNG","Epochs","Kendall-Tau",kendall,0,x_axis)
-        # create_plot("Average Kendall-Tau with original list","plt/kt1_orig_init.PNG","Epochs","Kendall-Tau",kendall,1,x_axis)
-        # create_plot("Average RBO measure with original list","plt/rbo1_min_orig_init.PNG","Epochs","RBO",rbo_min,1,x_axis)
-        # create_plot("Average RBO measure with last iteration","plt/rbo1_min_init.PNG","Epochs","RBO",rbo_min,0,x_axis)
-        # create_plot("Number of queries with winner changed", "plt/winner_change1_init.PNG", "Epochs", "#Queries",cr,0, x_axis)
-        #deltas = self.get_average_epsilon(number_of_competitors=5,scores=scores)
-        #create_plot("Average epsilon by epoch", "plt/eps.PNG", "Epochs", "Average epsilon", deltas, 0,  range(1,9))
-        self.extract_score(scores)
-        metrics=self.calculate_metrics(scores)
-        with open("comp.pickle",'wb') as f:
-          pickle.dump(metrics,f)
-        # with open("comp.pickle",'rb') as f:
-        #     metrics = pickle.load(f)
-        #     create_plot("NDCG@5 by epochs", "plt/ndcg_eps.png", "Epochs", "NDCG@5",metrics,0, range(1,9))
-        #     create_plot("map@5 by epochs", "plt/map_eps.png", "Epochs", "map@5",metrics,1, range(1,9))
+        if retrieval:
+            kendall, cr,rbo_min,x_axis = self.calculate_average_kendall_tau(rankings_svm)
+            create_plot("Average Kendall-Tau with last iteration","plt/kt1_init.PNG","Epochs","Kendall-Tau",kendall,0,x_axis)
+            create_plot("Average Kendall-Tau with original list","plt/kt1_orig_init.PNG","Epochs","Kendall-Tau",kendall,1,x_axis)
+            create_plot("Average RBO measure with original list","plt/rbo1_min_orig_init.PNG","Epochs","RBO",rbo_min,1,x_axis)
+            create_plot("Average RBO measure with last iteration","plt/rbo1_min_init.PNG","Epochs","RBO",rbo_min,0,x_axis)
+            create_plot("Number of queries with winner changed", "plt/winner_change1_init.PNG", "Epochs", "#Queries",cr,0, x_axis)
+            deltas = self.get_average_epsilon(number_of_competitors=5,scores=scores)
+            create_plot("Average epsilon by epoch", "plt/eps.PNG", "Epochs", "Average epsilon", deltas, 0,  range(1,9))
+            with open("comp.pickle", 'rb') as f:
+                metrics = pickle.load(f)
+                create_plot("NDCG@5 by epochs", "plt/ndcg_eps.png", "Epochs", "NDCG@5", metrics, 0, range(1, 9))
+                create_plot("map@5 by epochs", "plt/map_eps.png", "Epochs", "map@5", metrics, 1, range(1, 9))
+        else:
+            self.extract_score(scores)
+            metrics=self.calculate_metrics(scores)
+            with open("comp.pickle",'wb') as f:
+                pickle.dump(metrics,f)
 
