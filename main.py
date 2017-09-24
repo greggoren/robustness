f1809ab1d61ffd7c1386e08701145d062b307cf1
gregory goren <grggoren@gmail.com>
2017-09-24 11:28:34 +0300

    initial weights addition

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ if __name__=="__main__":
     analyze = a.analysis()
 
     # mhs= [("model_handler_ent_opt_minus.pickle0.001",'0.001','b'),("model_handler_ent_opt_minus.pickle0.01",'0.01','g'),("model_handler_ent_opt_minus.pickle0.1",'0.1','r'),("model_handler_ent_opt_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs= [("model_handler_ent_opt_shrinked_minus.pickle0.001",'0.001','b'),("model_handler_ent_opt_shrinked_minus.pickle0.01",'0.01','g'),("model_handler_ent_opt_shrinked_minus.pickle0.1",'0.1','r'),("model_handler_ent_opt_shrinked_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    mhs= [("model_handler_pos_minus.pickle0.001",'0.001','b'),("model_handler_pos_minus.pickle0.01",'0.01','g'),("model_handler_pos_minus.pickle0.1",'0.1','r'),("model_handler_pos_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
+    mhs= [("model_handler_ent_opt_shrinked_minus.pickle0.001",'0.001','b'),("model_handler_ent_opt_shrinked_minus.pickle0.01",'0.01','g'),("model_handler_ent_opt_shrinked_minus.pickle0.1",'0.1','r'),("model_handler_ent_opt_shrinked_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
+    # mhs= [("model_handler_pos_minus.pickle0.001",'0.001','b'),("model_handler_pos_minus.pickle0.01",'0.01','g'),("model_handler_pos_minus.pickle0.1",'0.1','r'),("model_handler_pos_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
     # mhs= [("model_handler_shrinked_pos_minus.pickle0.001",'0.001','b'),("model_handler_shrinked_pos_minus.pickle0.01",'0.01','g'),("model_handler_shrinked_pos_minus.pickle0.1",'0.1','r'),("model_handler_shrinked_pos_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
     mh_svm = preprocess.load_model_handlers(mhs)
     cd = preprocess.extract_features_by_epoch("data/features_asr")
