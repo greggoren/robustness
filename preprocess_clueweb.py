686b883ce8fff41d7dd9ef0ffef6acce650fc2fa
gregory goren <grggoren@gmail.com>
2017-10-30 22:51:17 +0200

    initial weights addition

--- preprocess_clueweb.py
+++ preprocess_clueweb.py
@@ -90,7 +90,7 @@ class preprocess:
         with open(train_file,'w') as feature_file:
             for i,doc in enumerate(X):
                 features = " ".join([str(a+1)+":"+str(b) for a,b in enumerate(doc)])
-                line = str(y[i])+" qid:"+queries[i]+" "+features+"\n"
+                line = str(y[i])+" qid:"+str(queries[i]).zfill(3)+" "+features+"\n"
                 feature_file.write(line)
         return train_file
 
