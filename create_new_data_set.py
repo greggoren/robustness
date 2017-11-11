0b4d6a3fb843d197722e41e5ef099cbc6228eeba
gregory goren <grggoren@gmail.com>
2017-11-11 11:59:36 +0200

    initial weights addition

--- create_new_data_set.py
+++ create_new_data_set.py
@@ -4,9 +4,14 @@ def read_original_data_set(data_set_file):
     with open(data_set_file) as data_set:
         for data in data_set:
             name = data.split(" # ")[1].rstrip()
-            query = data.split()[1].split("qid:")[0]
-            data_records[name] = data.split(" # ")[0]
-            queries[name]=query
+            queries[name]=[]
+            query = data.split()[1].replace("qid:","")
+            if not data_records.get(query,False):
+                data_records[query]={}
+            data_records[query][name] = data.split(" # ")[0]
+    for query in data_records:
+        for doc in data_records[query]:
+            queries[doc].append(query)
     return data_records,queries
 
 def retrieve_spam_score(spam_file,queries):
@@ -19,26 +24,29 @@ def retrieve_spam_score(spam_file,queries):
             if not queries.get(doc,False):
                 continue
             scores_tmp[doc]=score
-            if score>=maximum.get(queries[doc],score):
-                maximum[queries[doc]]=score
-            elif score<=minimum.get(queries[doc],score):
-                minimum[queries[doc]] = score
+            for query in queries[doc]:
+                if score>=maximum.get(query,score):
+                    maximum[query]=score
+                elif score<=minimum.get(query,score):
+                    minimum[query] = score
     scores={}
     for doc in scores_tmp:
-        query = queries[doc]
-        scores[doc]=float(scores_tmp[doc]-minimum[query])/(maximum[query]-minimum[query])
+        scores[doc]={}
+        for query in queries[doc]:
+            scores[doc][query]=float(scores_tmp[doc]-minimum[query])/(maximum[query]-minimum[query])
     return scores
 
 def create_data_set(data_records,scores):
     with open("ClueWeb09Extra",'w') as file:
-        for doc in data_records:
-            line = data_records[doc]+" 26:"+str(scores[doc])+" # "+doc+"\n"
-            file.write(line)
+        for query in data_records:
+            for doc in data_records[query]:
+                line = data_records[query][doc]+" 26:"+str(scores[doc][query])+" # "+doc+"\n"
+                file.write(line)
 
 
-data_records,queries=read_original_data_set("featuresCB_asr")
-scores=retrieve_spam_score("clueweb09spam.Fusion",queries)
+data_records,queries=read_original_data_set("data/featuresCB_asr")
+scores=retrieve_spam_score("debug",queries)
+#"clueweb09spam.Fusion"
 print("scores=",scores)
-print("data",data_records)
-
+print("queries",queries)
 create_data_set(data_records,scores)
