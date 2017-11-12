e80f5114af9d3d011eff0949e5996b7f1fee66af
gregory goren <grggoren@gmail.com>
2017-11-12 19:21:11 +0200

    initial weights addition

--- analyze_competition.py
+++ analyze_competition.py
@@ -760,7 +760,7 @@ class analysis:
         jar_path = "/lv_local/home/sgregory/SEO_CODE/model_running/RankLib.jar"
         score_file = "/lv_local/home/sgregory/robustness/score"+str(epoch)
         features= "/lv_local/home/sgregory/robustness/"+features
-        model_path  = "/lv_local/home/sgregory/robustness/model"
+        model_path  = "/lv_local/home/sgregory/robustness/testmodel_250_50"
         #os.popen('/bin/bash touch '+score_file)
         command = java_path+" -jar "+jar_path + " -load "+model_path+" -rank "+features+ " -score "+score_file
         run_bash_command(command)
--- comparison.py
+++ comparison.py
@@ -1,28 +1,31 @@
 import analyze_competition as a
 import prep as p
 import sys
+import svm_models_handler as mh
+import pickle
 
-if __name__=="__main__":
 
-    preprocess = p.preprocess()
-    analyze = a.analysis()
+# if __name__=="__main__":
 
-    # mhs= [("model_handler_ent_opt_minus.pickle0.001",'0.001','b'),("model_handler_ent_opt_minus.pickle0.01",'0.01','g'),("model_handler_ent_opt_minus.pickle0.1",'0.1','r'),("model_handler_ent_opt_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs= [("model_handler_ent_opt_shrinked_minus.pickle0.001",'0.001','b'),("model_handler_ent_opt_shrinked_minus.pickle0.01",'0.01','g'),("model_handler_ent_opt_shrinked_minus.pickle0.1",'0.1','r'),("model_handler_ent_opt_shrinked_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs= [("model_handler_pos_minus.pickle0.001",'0.001','b'),("model_handler_pos_minus.pickle0.01",'0.01','g'),("model_handler_pos_minus.pickle0.1",'0.1','r'),("model_handler_pos_minus.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs= [("abs_models/model_handler_abs.pickle0.1",'0.1','b'),("abs_models/model_handler_abs.pickle0.2",'0.2','g'),("abs_models/model_handler_abs.pickle0.01",'0.01','r'),("abs_models/model_handler_abs.pickle1.0",'1.0','m'),("abs_models/model_handler_abs.pickle10.0",'10.0','c'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs= [("model_handler_ent_opt_plus.pickle0.3",'0.3','b'),("model_handler_ent_opt_plus.pickle0.5",'0.5','g'),("model_handler_ent_opt_plus.pickle0.7",'0.1','r'),("model_handler_ent_opt_plus.pickle1.0",'1.0','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    mhs = [("regular/model_handler_asr_cmp.pickle", 'SVM', 'k')]
+preprocess = p.preprocess()
+analyze = a.analysis()
+svm = mh.models_handler([])
+model_file = open("svm_model",'rb')
+w = pickle.load(model_file)
+# svm.chosen_model_per_fold[1]=w
+for i in range(1,201):
+    svm.query_to_fold_index[i]=1
+for i in range(1,6):
+    svm.weights_index[i] = w
+mhs = [("regular/model_handler_asr_cmp.pickle", 'SVM', 'k')]
 
-    # mhs= [("model_handler_pos__sh1.pickle0.001",'0.001','b'),("model_handler_pos__sh1.pickle0.01",'0.01','g'),("model_handler_pos__sh1.pickle0.1",'0.1','r'),("model_handler_pos__sh1.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs= [("model_handler_ent_opt_shrinked1.pickle0.001",'0.001','b'),("model_handler_ent_opt_shrinked1.pickle0.01",'0.01','g'),("model_handler_ent_opt_shrinked1.pickle0.1",'0.1','r'),("model_handler_ent_opt_shrinked1.pickle0.2",'0.2','m'),("data/model_handler_asr_cmp.pickle",'svm','k')]
-    # mhs = [("regular/model_handler_asr_cmp.pickle", 'svm', 'k'),]
-    #              #("regular/model_handler_asr_cmp.pickle", 'svm_epsilon', 'k')]
-    mh_svm = preprocess.load_model_handlers(mhs)
-    cd = preprocess.extract_features_by_epoch("data/features_asr")
-    #analyze.analyze(mh_svm,cd,dump)
-    #analyze.create_table_for_epsilons(mh_svm,cd)
-    #analyze.create_rbo_baseline(mh_svm,cd)
-    # analyze.compare_rankers(mh_svm,cd)
-    # analyze.create_comparison_plots("results.pickle")
-    analyze.get_score_for_LambdaMart_on_clueWeb("data/featuresCB_asr")
+mh_svm = preprocess.load_model_handlers(mhs)
+print(mh_svm[0][0].query_to_fold_index)
+mh_svm=[(svm,"svm","SVM",'k')]
+cd = preprocess.extract_features_by_epoch("features_asr_modified")
+#analyze.analyze(mh_svm,cd,dump)
+#analyze.create_table_for_epsilons(mh_svm,cd)
+#analyze.create_rbo_baseline(mh_svm,cd)
+analyze.compare_rankers(mh_svm,cd)
+# analyze.create_comparison_plots("results.pickle")
+# analyze.get_score_for_LambdaMart_on_clueWeb("data/featuresCB_asr")
