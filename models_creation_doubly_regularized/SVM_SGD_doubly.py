a3580463f64c94ede319fb4ad31a278dd018ea6a
gregory goren <grggoren@gmail.com>
2017-11-22 18:32:59 +0200

    initial weights addition

---
+++ models_creation_doubly_regularized/SVM_SGD_doubly.py
@@ -0,0 +1,57 @@
+import SVM_SGD as svm_s
+import numpy as np
+import random as r
+import math
+from models_creation_doubly import params_L1
+import sys
+class svm_sgd_doubly(svm_s.svm_sgd):
+    def __init__(self, Lambda1=None,Lambda2=None):
+        self.Lambda1 = Lambda1
+        self.Lambda2 = Lambda2
+        self.w = None
+
+
+
+    def L1_norm_subgradient(self,number_of_features):
+        subgradient = np.zeros(number_of_features)
+        for i,w_i in enumerate(self.w):
+            if w_i<0:
+                subgradient[i]=-1
+            else:
+                subgradient[i]=1
+        return subgradient
+
+
+    def fit(self,X,y):
+        print ("started SGD")
+        Lambda1=self.Lambda1
+        number_of_examples,number_of_features = len(X),len(X[0])
+        if self.Lambda1 is None:
+            Lambda1 = 1.0/math.sqrt(number_of_examples)
+        self.w = np.zeros(number_of_features)#weights initialization
+        iterations = params_L1.iter_factor * number_of_examples
+        for t in range(iterations):#itarating over examples
+            if t%1000000==0:
+                print ("in iteration",t,"out of",iterations)
+                sys.stdout.flush()
+            lr = 1.0/(t+1)
+            random_index = r.randint(0,number_of_examples-1)
+            y_k = X[random_index]*y[random_index]
+            if not self.check_prediction(y_k):
+                self.w = self.w-lr*Lambda1*self.L1_norm_subgradient(number_of_features) + lr*y_k - lr*self.Lambda2*self.w
+            else:
+                self.w = self.w-lr*Lambda1*self.L1_norm_subgradient(number_of_features)- lr*self.Lambda2*self.w
+        print ("SGD ended")
+
+
+    def predict(self,X,queries,test_indices,eval,validation=None):
+        results = {}
+        for index in test_indices:
+            results[index] = np.dot(self.w,X[index].T)
+        return eval.create_trec_eval_file(test_indices,queries,results,str(self.Lambda),validation)
+
+    def predict_opt(self,X,queries,test_indices,eval,score,validation=None):
+        results = {}
+        for index in test_indices:
+            results[index] = np.dot(self.w,X[index].T)
+        return eval.create_trec_eval_file_opt(test_indices,queries,results,str(self.Lambda1)+"_"+str(self.Lambda2),score,self.Lambda1,self.Lambda2,validation)
---
+++ models_creation_doubly_regularized/one_model_chooser_svm_doubly.py
@@ -0,0 +1,38 @@
+from models_creation_doubly_regularized import preprocess_clueweb as p
+from models_creation_doubly_regularized import single_model_handler_svm_doubly as mh
+from models_creation_doubly_regularized import evaluator_doubly as e
+from models_creation_doubly_regularized import params_doubly
+import sys
+import random as r
+
+if __name__=="__main__":
+    r.seed(params_doubly.random_seed)  # traceability reasons
+    labda1 = sys.argv[1]
+    labda2 = float(sys.argv[2])
+    if labda1=="N":
+        labda=None
+    else:
+        labda1 = float(labda1)
+    preprocess = p.preprocess()
+    score_file = params_doubly.score_file
+    X,y,queries=preprocess.retrieve_data_from_file(params_doubly.data_set_file,params_doubly.normalized)
+    sys.stdout.flush()
+    number_of_queries = len(set(queries))
+    evaluator = e.eval()
+    evaluator.create_index_to_doc_name_dict()
+    evaluator.remove_score_file_from_last_run(score_file)
+    sys.stdout.flush()
+    train,validation = preprocess.create_test_train_split_cluweb(queries)
+    sys.stdout.flush()
+    X_i,y_i=preprocess.create_data_set(X[train], y[train], queries[train])
+    sys.stdout.flush()
+    Lambda1_array = [labda1]
+    Lambda2_array = [labda2]
+    single_model_handler = mh.single_model_handler_svm_L1(Lambda1_array,Lambda2_array)
+    single_model_handler.fit_model_on_train_set_and_choose_best_for_competition(X,y,X_i,y_i,validation,queries,evaluator,preprocess,score_file)
+    print("learning is finished")
+
+
+
+
+
