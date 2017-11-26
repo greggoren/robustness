4850b7dffc2947393f8faeaf25506bea6615c4e7
gregory goren <grggoren@gmail.com>
2017-11-26 10:29:42 +0200

    initial weights addition

---
+++ models_creation_L1_regular/one_model_chooser_svm_L1.py
@@ -0,0 +1,36 @@
+from models_creation_L1_regular import preprocess_clueweb as p
+from models_creation_L1_regular import single_model_handler_svm_L1 as mh
+from models_creation_L1_regular import evaluator_L1 as e
+from models_creation_L1_regular import params_L1
+import sys
+import random as r
+
+if __name__=="__main__":
+    r.seed(params_L1.random_seed)  # traceability reasons
+    labda = sys.argv[1]
+    if labda=="N":
+        labda=None
+    else:
+        labda = float(labda)
+    preprocess = p.preprocess()
+    score_file = params_L1.score_file
+    X,y,queries=preprocess.retrieve_data_from_file(params_L1.data_set_file,params_L1.normalized)
+    sys.stdout.flush()
+    number_of_queries = len(set(queries))
+    evaluator = e.eval()
+    evaluator.create_index_to_doc_name_dict()
+    evaluator.remove_score_file_from_last_run(score_file)
+    sys.stdout.flush()
+    train,validation = preprocess.create_test_train_split_cluweb(queries)
+    sys.stdout.flush()
+    X_i,y_i=preprocess.create_data_set(X[train], y[train], queries[train])
+    sys.stdout.flush()
+    Lambda_array = [labda]
+    single_model_handler = mh.single_model_handler_svm_L1(Lambda_array)
+    single_model_handler.fit_model_on_train_set_and_choose_best_for_competition(X,y,X_i,y_i,validation,queries,evaluator,preprocess,score_file)
+    print("learning is finished")
+
+
+
+
+
---
+++ models_creation_L1_regular/single_model_handler_svm_L1.py
@@ -0,0 +1,39 @@
+from models_creation_L1_regular import SVM_SGD_L1
+import operator
+import pickle
+from models_creation_L1_regular import params_L1
+
+
+class single_model_handler_svm_L1():
+
+    def __init__(self, Lambda_array):
+        self.models = {}
+        for Lambda in Lambda_array:
+            self.models[Lambda]=SVM_SGD_L1.svm_sgd_L1(Lambda)
+
+
+    def fit_model_on_train_set_and_choose_best_for_competition(self,X,y,X_i,y_i,validation_indices,queries,evaluator,preprocess,score_file):
+        evaluator.empty_validation_files(params_L1.validation_folder)
+        weights = {}
+        scores={}
+        for Lambda in self.models:
+            print("fitting model on Lambda=", Lambda)
+            svm = self.models[Lambda]
+            svm.fit(X_i,y_i)
+            weights[svm.Lambda]=svm.w
+            score_file = svm.predict_opt(X, queries, validation_indices,evaluator, score_file,True)
+            ordered_trec_file = evaluator.order_trec_file(score_file)
+            score = evaluator.run_trec_eval(ordered_trec_file)
+            scores[svm.Lambda] = score
+            print("weights=",[str(round(a,3)) for a in svm.w])
+        max_Lambda=max(scores.items(), key=operator.itemgetter(1))[0]
+        print("the chosen model is Lambda=",max_Lambda)
+        chosen_model = self.models[max_Lambda]
+        data_set,tags=preprocess.create_data_set(X, y, queries)
+        chosen_model.fit(data_set,tags)
+
+        with open("svm_model_L1.pickle"+str(max_Lambda),'wb') as model_file:
+            pickle.dump(chosen_model,model_file)
+
+
+
