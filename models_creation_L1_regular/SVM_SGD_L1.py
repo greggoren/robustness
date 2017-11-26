587aa7d45037ecc3e863492999317a021d499ee4
gregory goren <grggoren@gmail.com>
2017-11-26 14:08:45 +0200

    initial weights addition

--- models_creation_L1_regular/SVM_SGD_L1.py
+++ models_creation_L1_regular/SVM_SGD_L1.py
@@ -11,9 +11,9 @@ class svm_sgd_L1(svm_s.svm_sgd):
 
 
 
-    def L1_norm_subgradient(self,number_of_features):
+    def L1_norm_subgradient(self,number_of_features,w):
         subgradient = np.zeros(number_of_features)
-        for i,w_i in enumerate(self.w):
+        for i,w_i in enumerate(w):
             if w_i<0:
                 subgradient[i]=-1
             else:
@@ -28,6 +28,8 @@ class svm_sgd_L1(svm_s.svm_sgd):
         if self.Lambda is None:
             Lambda = 1.0/math.sqrt(number_of_examples)
         w = np.zeros(number_of_features)#weights initialization
+        self.w = np.zeros(number_of_features)#weights initialization
+
         iterations = params_L1.iter_factor * number_of_examples
         for t in range(iterations):#itarating over examples
             if t%1000000==0:
@@ -36,15 +38,20 @@ class svm_sgd_L1(svm_s.svm_sgd):
             lr = 1.0/(t+1)
             random_index = r.randint(0,number_of_examples-1)
             y_k = X[random_index]*y[random_index]
-            if not self.check_prediction(y_k):
-                w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features) + lr*y_k*number_of_examples
+            if not self.check_prediction(y_k,w):
+                w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features,w) + lr*y_k*number_of_examples
             else:
-                w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features)
+                w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features,w)
             self.w+=w
         self.w=self.w*(1.0/iterations)
 
         print ("SGD ended")
 
+    def check_prediction(self,y_k,w):
+
+        if np.dot(w,y_k.T)>1:
+            return True
+        return False
 
     def predict(self,X,queries,test_indices,eval,validation=None):
         results = {}
