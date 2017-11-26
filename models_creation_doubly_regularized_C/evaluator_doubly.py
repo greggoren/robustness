de733184c984f47257779e573e6ff7296d01c276
gregory goren <grggoren@gmail.com>
2017-11-26 17:46:56 +0200

    initial weights addition

---
+++ models_creation_doubly_regularized_C/evaluator_doubly.py
@@ -0,0 +1,135 @@
+import shutil
+import subprocess
+import os
+from models_creation_doubly import params_L1 as params_l1
+
+
+class eval:
+
+
+    def __init__(self):
+        self.metrics = ["map","ndcg_cut.20","P.5","P.10"]
+        self.validation_metric = "ndcg_cut.20"
+        self.doc_name_index = {}
+
+    def remove_score_file_from_last_run(self,score_file):
+        command = "rm "+score_file
+        for line in self.run_command(command):
+            print(line)
+
+    def create_trec_eval_file(self, test_indices, queries, results,model,validation=None):#TODO: need to sort file via unix command
+        if validation is not None:
+            trec_file = params_l1.validation_folder + "/trec_file_" + model + ".txt"
+            if not os.path.exists(os.path.dirname(trec_file)):
+                os.makedirs(os.path.dirname(trec_file))
+
+        else:
+            trec_file = params_l1.score_file
+        trec_file_access = open(trec_file,'a')
+        for index in test_indices:
+            trec_file_access.write(self.set_qid_for_trec(queries[index])+" Q0 "+self.doc_name_index[index]+" "+str(0)+" "+str(results[index])+" seo\n")
+        trec_file_access.close()
+        return trec_file
+
+
+
+    def create_trec_eval_file_opt(self, test_indices, queries, results,model,score,Lambda1,Lambda2,C,validation=None):
+        if validation is not None:
+            trec_file = params_l1.validation_folder + "/" + str(Lambda1)+"_"+str(Lambda2)+"_"+str(C) + "/trec_file_" + model + ".txt"
+            if not os.path.exists(os.path.dirname(trec_file)):
+                os.makedirs(os.path.dirname(trec_file))
+
+        else:
+            trec_file = score
+        trec_file_access = open(trec_file,'a')
+        for index in test_indices:
+            trec_file_access.write(self.set_qid_for_trec(queries[index])+" Q0 "+self.doc_name_index[index]+" "+str(0)+" "+str(results[index])+" seo\n")
+        trec_file_access.close()
+        return trec_file
+
+    def order_trec_file(self,trec_file):
+        final = trec_file.replace(".txt","")
+        command = "sort -k1,1 -k5nr -k2,1 "+trec_file+" > "+final
+        for line in self.run_command(command):
+            print(line)
+        return final
+
+    def run_command(self, command):
+        p = subprocess.Popen(command,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             shell=True)
+        return iter(p.stdout.readline, b'')
+
+    def run_trec_eval(self, score_file):
+        command = "./trec_eval -m " + self.validation_metric + " " + params_l1.qrels + " " + score_file
+        for output_line in self.run_command(command):
+            print("output line=",output_line)
+            score = output_line.split()[-1].rstrip()
+            break
+        return score
+
+    def empty_validation_files(self,validation_folder):
+        try:
+            shutil.rmtree(validation_folder)
+        except:
+            print("no validation folder")
+
+
+    def run_trec_eval_on_test(self):
+        score_data = []
+        print("last stats:")
+        for metric in self.metrics:
+            command = "./trec_eval -m " + metric + " " + params_l1.qrels + " " + params_l1.score_file
+            for output_line in self.run_command(command):
+                print(metric,output_line)
+                score = output_line.split()[-1].rstrip()
+                score_data.append((metric, str(score)))
+        summary_file = open(params_l1.summary_file, 'w')
+        summary_file.write("METRIC\tSCORE\n")
+        for score_record in score_data:
+            next_line = score_record[0] + "\t" + score_record[1] + "\n"
+            summary_file.write(next_line)
+        summary_file.close()
+
+    def run_trec_eval_on_test_for_opt(self,score_file,summary):
+        score_data = []
+        print("last stats:")
+        for metric in self.metrics:
+            command = "./trec_eval -m " + metric + " " + params_l1.qrels + " " + score_file
+            for output_line in self.run_command(command):
+                print(metric,output_line)
+                score = output_line.split()[-1].rstrip()
+                score_data.append((metric, str(score)))
+        summary_file = open(summary, 'w')
+        summary_file.write("METRIC\tSCORE\n")
+        for score_record in score_data:
+            next_line = score_record[0] + "\t" + score_record[1] + "\n"
+            summary_file.write(next_line)
+        summary_file.close()
+
+    def create_index_to_doc_name_dict(self):
+        index =0
+        with open(params_l1.data_set_file) as ds:
+            for line in ds:
+                rec = line.split("# ")
+                doc_name = rec[1].rstrip()
+                self.doc_name_index[index]=doc_name
+                index+=1
+
+    def set_qid_for_trec(self,query):
+        if query < 10:
+            qid = "00" + str(query)
+        elif query < 100:
+            qid = "0" + str(query)
+        else:
+            qid = str(query)
+        return qid
+
+    def create_qrels_file(self,X,y,queries):
+        print("creating qrels file")
+        qrels = open(params_l1.qrels, 'w')
+        for i in range(len(X)):
+            qrels.write(self.set_qid_for_trec(queries[i]) + " 0 " + self.doc_name_index[i] + " " + str(int(y[i])) + "\n")
+        qrels.close()
+        print("qrels file ended")
---
+++ models_creation_doubly_regularized_C/single_model_handler_svm_doubly.py
@@ -0,0 +1,41 @@
+from models_creation_doubly_regularized_C import SVM_SGD_doubly
+import operator
+import pickle
+from models_creation_doubly_regularized_C import params_doubly
+
+
+class single_model_handler_svm_doubly():
+
+    def __init__(self, Lambda1_array,Lambda2_array,C_array):
+        self.models = {}
+        for Lambda1 in Lambda1_array:
+            for Lambda2 in Lambda2_array:
+                for C in C_array:
+                    self.models[(Lambda1,Lambda2,C)]=SVM_SGD_doubly.svm_sgd_doubly(Lambda1,Lambda2,C)
+
+
+    def fit_model_on_train_set_and_choose_best_for_competition(self,X,y,X_i,y_i,validation_indices,queries,evaluator,preprocess,score_file):
+        evaluator.empty_validation_files(params_doubly.validation_folder)
+        weights = {}
+        scores={}
+        for Lambda1,Lambda2,C in self.models:
+            print("fitting model on Lambda1=", Lambda1,"Lambda2=",Lambda2,"C=",C)
+            svm = self.models[(Lambda1,Lambda2,C)]
+            svm.fit(X_i,y_i)
+            weights[(svm.Lambda1,svm.Lambda2,svm.C)]=svm.w
+            score_file = svm.predict_opt(X, queries, validation_indices,evaluator, score_file,True)
+            ordered_trec_file = evaluator.order_trec_file(score_file)
+            score = evaluator.run_trec_eval(ordered_trec_file)
+            scores[(svm.Lambda1,svm.Lambda2,svm.C)] = score
+            print("weights=",[str(round(a,3)) for a in svm.w])
+        max_Lambda1,max_Lambda2,max_C=max(scores.items(), key=operator.itemgetter(1))[0]
+        print("the chosen model is Lambda1=", max_Lambda1,"Lambda2=",max_Lambda2,"C=",max_C)
+        chosen_model = self.models[(max_Lambda1,max_Lambda2,max_C)]
+        data_set,tags=preprocess.create_data_set(X, y, queries)
+        chosen_model.fit(data_set,tags)
+
+        with open("svm_model_L1.pickle"+str(C)+"_"+str(max_Lambda1)+"_"+str(max_Lambda2),'wb') as model_file:
+            pickle.dump(chosen_model,model_file)
+
+
+
