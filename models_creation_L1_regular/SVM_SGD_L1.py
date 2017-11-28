444e7a37c71085d5de888f81fa3f4ea230073db9
gregory goren <grggoren@gmail.com>
2017-11-28 20:26:53 +0200

    initial weights addition

--- models_creation_L1_regular/SVM_SGD_L1.py
+++ models_creation_L1_regular/SVM_SGD_L1.py
@@ -43,6 +43,39 @@ class svm_sgd_L1(svm_s.svm_sgd):
                 self.w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features)
         print ("SGD ended")
 
+    def check_validation(self,validation,tags,X):
+        hinge_loss=0
+        norm=0
+        for index in validation[:10000]:
+            tmp=self.w*X[index]*tags[index]
+            if tmp<1:
+                hinge_loss+=(1-tmp)
+        for w_i in self.w:
+            norm+=abs(w_i)
+        return self.Lambda*norm+self.C*hinge_loss
+
+    def fit_final(self,X,y,validation):
+        print ("started SGD")
+        Lambda=self.Lambda
+        number_of_examples,number_of_features = len(X),len(X[0])
+        if self.Lambda is None:
+            Lambda = 1.0/math.sqrt(number_of_examples)
+        self.w = np.zeros(number_of_features)#weights initialization
+        iterations = params_L1.iter_factor * number_of_examples
+        for t in range(iterations):#itarating over examples
+            if t%1000000==0:
+                print ("in iteration",t,"out of",iterations)
+                loss = self.check_validation(validation,y,X)
+                print("loss is ",loss)
+                sys.stdout.flush()
+            lr = 1.0/(t+1)
+            random_index = r.randint(0,number_of_examples-1)
+            y_k = X[random_index]*y[random_index]
+            if not self.check_prediction(y_k):
+                self.w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features) + lr*y_k*number_of_examples*self.C
+            else:
+                self.w = self.w-lr*Lambda*self.L1_norm_subgradient(number_of_features)
+        print ("SGD ended")
 
     def predict(self,X,queries,test_indices,eval,validation=None):
         results = {}
