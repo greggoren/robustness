11c38b1325fc1ec02f474d5e12f0f9f24a85c184
gregory goren <grggoren@gmail.com>
2017-11-28 15:42:42 +0200

    initial weights addition

--- models_creation_L1_regular/single_model_handler_svm_L1.py
+++ models_creation_L1_regular/single_model_handler_svm_L1.py
@@ -26,13 +26,13 @@ class single_model_handler_svm_L1():
             ordered_trec_file = evaluator.order_trec_file(score_file)
             score = evaluator.run_trec_eval(ordered_trec_file)
             scores[(svm.Lambda,svm.C)] = score
-            print("weights=",[str(round(a,3)) for a in svm.w])
+
         max_Lambda,max_C=max(scores.items(), key=operator.itemgetter(1))[0]
         print("the chosen model is Lambda=",max_Lambda,"C=",max_C)
         chosen_model = self.models[(max_Lambda,max_C)]
         data_set,tags=preprocess.create_data_set(X, y, queries)
         chosen_model.fit(data_set,tags)
-
+        print("weights=", [str(round(a, 3)) for a in chosen_model.w])
         with open("svm_model_L1.pickle"+str(max_C)+"_"+str(max_Lambda),'wb') as model_file:
             pickle.dump(chosen_model,model_file)
 
