c450b396f971f70965bca4178532f38339b06ad8
gregory goren <grggoren@gmail.com>
2018-01-02 19:56:03 +0200

    initial weights addition

--- bias_variance_tradeoff/analyze.py
+++ bias_variance_tradeoff/analyze.py
@@ -133,8 +133,9 @@ class analyze:
                         last_list_index_svm[query]=current_list_svm
                         original_list_index_svm[query]=current_list_svm
                         continue
-                    if current_list_svm.index(len(current_list_svm)) != last_list_index_svm[query].index(
-                            len(last_list_index_svm[query])):
+                    # if current_list_svm.index(len(current_list_svm)) != last_list_index_svm[query].index(
+                    if current_list_svm.index(5) != last_list_index_svm[query].index(
+                            5):
                         change_rate_svm += 1
                     n_q += 1
                     kt = kendalltau(last_list_index_svm[query], current_list_svm)[0]
