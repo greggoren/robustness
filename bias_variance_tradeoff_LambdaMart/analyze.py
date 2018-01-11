7ea623596ebdbd866097012be06f8b5ce7fbcdae
gregory goren <grggoren@gmail.com>
2018-01-11 17:07:23 +0200

    initial weights addition

--- bias_variance_tradeoff_LambdaMart/analyze.py
+++ bias_variance_tradeoff_LambdaMart/analyze.py
@@ -81,7 +81,7 @@ class analyze:
                 name = model.split("model_")[1]
 
                 score_file = name + "_" + str(i)
-                qrels = "../rel2/rel0" + str(i)
+                qrels = "../rel3/rel0" + str(i)
 
                 command = "../trec_eval -m ndcg "+qrels+" "+score_file
                 for line in run_command(command):
