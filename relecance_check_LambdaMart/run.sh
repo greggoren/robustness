3e2afc26109f5ff11ecda69b9e1502078aab655b
gregory goren <grggoren@gmail.com>
2018-01-13 12:31:18 +0200

    initial weights addition

--- relecance_check_LambdaMart/run.sh
+++ relecance_check_LambdaMart/run.sh
@@ -1,9 +1,9 @@
-nohup python LambdaMart_crossvalidation.py 200 150 > 20_5.log &
-nohup python LambdaMart_crossvalidation.py 200 200 > 30_5.log &
-nohup python LambdaMart_crossvalidation.py 200 250 > 40_5.log &
-nohup python LambdaMart_crossvalidation.py 200 300 > 50_5.log &
-nohup python LambdaMart_crossvalidation.py 200 350 > 60_5.log &
-nohup python LambdaMart_crossvalidation.py 200 450 > 70_5.log &
-nohup python LambdaMart_crossvalidation.py 200 500 > 70_5.log &
+nohup python LambdaMart_crossvalidation.py 200 550 > 20_5.log &
+nohup python LambdaMart_crossvalidation.py 200 600 > 30_5.log &
+nohup python LambdaMart_crossvalidation.py 200 700 > 40_5.log &
+nohup python LambdaMart_crossvalidation.py 200 750 > 50_5.log &
+#nohup python LambdaMart_crossvalidation.py 200 350 > 60_5.log &
+#nohup python LambdaMart_crossvalidation.py 200 450 > 70_5.log &
+#nohup python LambdaMart_crossvalidation.py 200 500 > 70_5.log &
 
 
