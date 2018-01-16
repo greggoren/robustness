b63b6416304adf67eee339ee6f6db16b6a3b7287
gregory goren <grggoren@gmail.com>
2018-01-16 09:15:49 +0200

    initial weights addition

--- results_test.py
+++ results_test.py
@@ -1,41 +1,11 @@
-import preprocess_clueweb as p
-import svm_ent_models_handler as mh
-import evaluator_ent as e
-import numpy as np
-import params_ent
-import pickle
-def fit_models(X, y, svm):
-    svm.fit(X, y)
-    return svm
+bins = {}
+jumps = 0.1
+start = 0
+end = 0.1
+for i in range(10):
+    bins[(start, end)] = 0
+    start = round(end, 3)
+    end += jumps
+    end = round(end, 3)
 
-
-def set_qid_for_trec(query):
-    if query < 10:
-        qid = "00" + str(query)
-    elif query < 100:
-        qid = "0" + str(query)
-    else:
-        qid = str(query)
-    return qid
-
-
-if __name__=="__main__":
-
-    preprocess = p.preprocess()
-    X,y,queries=preprocess.retrieve_data_from_file("features",False)
-    evaluator = e.eval()
-    evaluator.create_index_to_doc_name_dict()
-    file = open("model_handler_ent_opt.pickle",'rb')
-    file2 = ""
-    mh = pickle.load(file)
-
-    """with open("test_res.txt",'w') as f:
-        for index in range(len(X)):
-            q = queries[index]
-            fold = mh.query_to_fold_index[q]
-            weights= mh.weights_index[fold]
-            f.write(set_qid_for_trec(queries[index])+" Q0 "+evaluator.doc_name_index[index]+" "+str(0)+" "+str(np.dot(X[index],weights.T))+" seo\n")"""
-
-
-
-
+print(bins)
---
+++ winner_change_similarity_check/get_lm_res.py
@@ -0,0 +1,15 @@
+import os
+from  winner_change_similarity_check import analyze as a
+import prep as p
+import numpy as np
+import pickle
+
+if __name__ == "__main__":
+    preprocess = p.preprocess()
+    analyze = a.analyze()
+    model_file = open("../svm_model", 'rb')
+    banned_queries = []  # get_banned("banned")
+
+    models = {"../testmodel250_50": "../testmodel250_50"}
+    competition_data = preprocess.extract_features_by_epoch("../features_asr_modified")
+    analyze.create_table(competition_data, models, [])
