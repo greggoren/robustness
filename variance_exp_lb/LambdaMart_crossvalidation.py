1ba73c8c8fb6b8ddbd3323551855504381160710
gregory goren <grggoren@gmail.com>
2018-01-25 01:17:30 +0200

    initial weights addition

--- variance_exp_lb/LambdaMart_crossvalidation.py
+++ variance_exp_lb/LambdaMart_crossvalidation.py
@@ -27,23 +27,23 @@ if __name__ == "__main__":
     evaluator = e.eval()
     evaluator.create_index_to_doc_name_dict()
     evaluator.remove_score_file_from_last_run()
-    folds = preprocess.create_folds(X, y, queries, 5)
     fold_number = 1
-    trees = 250
-    leaves = [(i + 1) * 10 for i in range(17, 26)]
-    scores = {(trees, leaf): {i: [] for i in range(len(queries))} for leaf in leaves}
+    trees = [(i + 1) * 10 for i in range(17, 26)]
+    leaves = 50
+    scores = {(trees, leaves): {i: [] for i in range(len(queries))} for tree in trees}
 
-    for leaf in leaves:
+    for tree in trees:
+        folds = preprocess.create_folds(X, y, queries, 5)
         for train, test in folds:
-            model_handler = mh.model_handler_LambdaMart(trees, leaf)
-            with Pool(processes=5) as pool:
+            model_handler = mh.model_handler_LambdaMart(tree, leaves)
+            with Pool(processes=10) as pool:
                 model_handler.set_queries_to_folds(queries, test, fold_number)
                 f = functools.partial(model_handler.fit_model_on_train_set_for_variance, params.qrels, fold_number)
                 score_files = pool.map(f, range(1, 31))
                 for score in score_files:
                     subset = int(score.split("#")[1])
                     results = get_results(score, test)
-                    scores = update_scores(results, scores, subset, trees, leaf)
+                    scores = update_scores(results, scores, subset, tree, leaves)
             fold_number += 1
     print("it took:", time.time() - start)
     with open("variance_data1", 'wb') as data:
