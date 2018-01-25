0064bf74b3fd7b5c79fdd66d1178ae8c8cf1ea63
gregory goren <grggoren@gmail.com>
2018-01-25 15:53:46 +0200

    initial weights addition

--- bias_variance_tradeoff_LambdaMart/lambdamart_simple_model_creator.py
+++ bias_variance_tradeoff_LambdaMart/lambdamart_simple_model_creator.py
@@ -8,12 +8,36 @@ if __name__ == "__main__":
     preprocess = p.preprocess()
     # trees = [(i + 1) * 10 for i in range(15, 46)]
     trees = 250
-    leaves = [(1 + i) * 5 for i in range(10)]
+    leaves = [(1 + i) * 10 for i in range(5)]
+    single_model_handler = mh.single_model_handler_LambdaMart(0, trees)
+    qrels = params.qrels
+    train_file = params.data_set_file
+    f = partial(single_model_handler.create_model_LambdaMart, trees, train_file, qrels)
+    with Pool(processes=5) as pool:
+        # single_model_handler.create_model_LambdaMart(trees, )
+        pool.map(f, leaves)
+    leaves = [(1 + i) * 10 for i in range(5, 10)]
+    single_model_handler = mh.single_model_handler_LambdaMart(0, trees)
+    qrels = params.qrels
+    train_file = params.data_set_file
+    f = partial(single_model_handler.create_model_LambdaMart, trees, train_file, qrels)
+    with Pool(processes=3) as pool:
+        # single_model_handler.create_model_LambdaMart(trees, )
+        pool.map(f, leaves)
+    leaves = [(1 + i) * 10 for i in range(10, 13)]
     single_model_handler = mh.single_model_handler_LambdaMart(leaves, trees)
     qrels = params.qrels
     train_file = params.data_set_file
     f = partial(single_model_handler.create_model_LambdaMart, trees, train_file, qrels)
-    with Pool(processes=10) as pool:
+    with Pool(processes=2) as pool:
+        # single_model_handler.create_model_LambdaMart(trees, )
+        pool.map(f, leaves)
+    leaves = [(1 + i) * 10 for i in range(13, 25)]
+    single_model_handler = mh.single_model_handler_LambdaMart(leaves, trees)
+    qrels = params.qrels
+    train_file = params.data_set_file
+    f = partial(single_model_handler.create_model_LambdaMart, trees, train_file, qrels)
+    with Pool(processes=1) as pool:
         # single_model_handler.create_model_LambdaMart(trees, )
         pool.map(f, leaves)
     print("learning is finished")
