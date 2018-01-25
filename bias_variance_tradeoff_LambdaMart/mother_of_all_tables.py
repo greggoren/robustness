257056d1d34c8fa9c13d8676463b68a0d8284c72
gregory goren <grggoren@gmail.com>
2018-01-25 21:41:38 +0200

    initial weights addition

--- bias_variance_tradeoff_LambdaMart/mother_of_all_tables.py
+++ bias_variance_tradeoff_LambdaMart/mother_of_all_tables.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
     # models = upload_models("models")
     # banned = get_banned("../banned1")
     banned = {i: [] for i in [1, 2, 3, 4, 5, 6, 7, 8]}
-    banned[2].append("164")
-    banned[2].append("010")
+    # banned[2].append("164")
+    # banned[2].append("010")
     models = ["../model_250_50"]
     competition_data = preprocess.extract_features_by_epoch("../features_asr_modified")
     # competition_data = preprocess.extract_features_by_epoch("../featuresASR_round1_LambdaMART")
