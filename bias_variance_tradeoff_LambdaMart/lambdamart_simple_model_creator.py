35ad13559684c2679dc455aa587f5b9915606989
gregory goren <grggoren@gmail.com>
2018-01-26 13:43:10 +0200

    initial weights addition

--- bias_variance_tradeoff_LambdaMart/lambdamart_simple_model_creator.py
+++ bias_variance_tradeoff_LambdaMart/lambdamart_simple_model_creator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     qrels = params.qrels
     train_file = params.data_set_file
     f = partial(single_model_handler.create_model_LambdaMart, leaves, train_file, qrels)
-    with Pool(processes=3) as pool:
+    with Pool(processes=6) as pool:
         # single_model_handler.create_model_LambdaMart(trees, )
-        pool.map(f, trees)
+        pool.map(f, zip(trees, trees))
     print("learning is finished")
