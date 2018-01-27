f0db04371b8ac9dea0da1f137771141d0aee41f6
gregory goren <grggoren@gmail.com>
2018-01-27 14:29:16 +0200

    -

--- bias_variance_tradeoff_svm/svmlight_simple_model_creator.py
+++ bias_variance_tradeoff_svm/svmlight_simple_model_creator.py
@@ -77,8 +77,9 @@ if __name__ == "__main__":
     #     model_file = learn_svm(C, train_file)
     #
     # C_array = [float(i * 1000) for i in range(1, 10)]
-    C_array = [0.0001, 0.001, 0.01, 0.1]
-    C_array.extend([(i + 1) * 40 for i in range(25)])
+    # C_array = [0.0001, 0.001, 0.01, 0.1]
+    C_array = []
+    C_array.extend([(i + 1) * 45 for i in range(25)])
     existing = upload_models("models_light")
     C_array = list(set(C_array) - set(existing))
     f = partial(learn_svm, train_file)
