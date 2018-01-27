066d9c3ed8e47d6bb04e32f8dca76794c57a7f15
gregory goren <grggoren@gmail.com>
2018-01-27 10:35:41 +0200

    -

--- bias_variance_tradeoff_svm/mother_of_all_tables.py
+++ bias_variance_tradeoff_svm/mother_of_all_tables.py
@@ -75,14 +75,14 @@ if __name__ == "__main__":
     C_array.extend([(i + 1) * 10 for i in range(5)])
     C_array.extend([(i + 1) * 100 for i in range(5)])
     # C_array.extend([900, 800, 600, 700])
-    svms = upload_models("models_light", C_array)
+    # svms = upload_models("models_light", C_array)
     preprocess = p.preprocess()
     analyze = a.analyze()
 
     banned = get_banned("../banned1")
     # banned = {i: [] for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]}
     # banned[2].append("164")
-    # svms = {"svm_model0.1": pickle.load(open("../svm_model", 'rb'))}
+    svms = {"svm_model0.1": pickle.load(open("../svm_model", 'rb'))}
     # competition_data = preprocess.extract_features_by_epoch("../features_asr_modified")
     competition_data = preprocess.extract_features_by_epoch("../featuresASR_round1_SVM")
     analyze.create_table(competition_data, svms, banned)
