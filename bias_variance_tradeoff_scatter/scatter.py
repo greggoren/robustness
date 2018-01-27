36634cc910a75d47c2c6d941cf212202e5958f64
gregory goren <grggoren@gmail.com>
2018-01-27 16:21:52 +0200

    -

--- bias_variance_tradeoff_scatter/scatter.py
+++ bias_variance_tradeoff_scatter/scatter.py
@@ -15,13 +15,17 @@ def create_scatter_plot(title, file_name, xlabel, ylabel, x, y):
 
 # C,mean,max = pickle.load(open("mean_max_c_w_kt",'rb'))
 # trees,mean,max = pickle.load(open("mean_max_trees_w_kt",'rb'))
-C, wc = pickle.load(open("kt", 'rb'))
+C, kt = pickle.load(open("kt", 'rb'))
+C, wc = pickle.load(open("wc", 'rb'))
 
 # create_scatter_plot("Weighted kendall tau (max aggregation) as function of C","max_36_45","C","Weighted KT",C[36:45],max[36:45])
 # create_scatter_plot("Weighted kendall tau (mean aggregation) as function of C","mean_36_45","C","Weighted KT",C[36:46],mean[36:46])
 # create_scatter_plot("Weighted kendall tau (mean aggregation) as function of #trees","mean_tress","#trees","Weighted KT",trees,mean)
 # create_scatter_plot("Weighted kendall tau (max aggregation) as function of #trees","max_tress","#trees","Weighted KT",trees,max)
-create_scatter_plot("KT as f(C)", "kt_scat_last", "C",
-                    "KT", C, wc)
+create_scatter_plot("WC as f(C) Competition - Part1 ", "wc_scat_1", "C",
+                    "WC", C, wc)
+create_scatter_plot("KT as f(C) Competition - Part1", "kt_scat_1", "C",
+                    "KT", C, kt)
+
 # create_scatter_plot("Weighted kendall tau (mean aggregation) as function of #leaves", "mean_leaves", "#leaves",
 #                     "Weighted KT", leaves, mean)
