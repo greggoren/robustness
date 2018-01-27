88a6479a116af49119ea53af7828c175d89fa5f1
gregory goren <grggoren@gmail.com>
2018-01-27 16:27:59 +0200

    -

--- bias_variance_tradeoff_LambdaMart/analyze.py
+++ bias_variance_tradeoff_LambdaMart/analyze.py
@@ -103,7 +103,7 @@ class analyze:
                 name = model.split("model_")[1]
 
                 score_file = name + "_" + str(i)
-                qrels = "../rel2/srel0" + str(i)
+                qrels = "../rel2/rel0" + str(i)
 
                 command = "../trec_eval -q -m ndcg " + qrels + " " + score_file
                 for line in run_command(command):
