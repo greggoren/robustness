d06d7871dfc80e6667056ac651728203815e841a
gregory goren <grggoren@gmail.com>
2018-01-28 10:23:53 +0200

    -

--- bias_variance_tradeoff_LambdaMart/mother_of_all_tables.py
+++ bias_variance_tradeoff_LambdaMart/mother_of_all_tables.py
@@ -18,18 +18,18 @@ def get_banned(banned_file):
     with open(banned_file) as banned:
         for ban in banned:
             splitted = ban.split()
-            banned_queries[int(splitted[0])].append(splitted[1])
+            banned_queries[int(splitted[0]) - 5].append(splitted[1])
     return banned_queries
 
 if __name__ == "__main__":
     preprocess = p.preprocess()
     analyze = a.analyze()
     models = upload_models("models")
-    banned = get_banned("../banned1")
+    banned = get_banned("../banned2")
     # banned = {i: [] for i in [1, 2, 3, 4, 5, 6, 7, 8]}
     # banned[2].append("164")
     # banned[2].append("010")
     # models = ["../model_250_50"]
     # competition_data = preprocess.extract_features_by_epoch("../features_asr_modified")
-    competition_data = preprocess.extract_features_by_epoch("../featuresASR_round1_LambdaMART")
+    competition_data = preprocess.extract_features_by_epoch("../featuresASR_round2_LambdaMART")
     analyze.create_table(competition_data, models, banned)
