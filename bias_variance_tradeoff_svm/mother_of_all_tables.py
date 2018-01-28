983c9eedbf6747249b927f59bd3ad8620bc2a900
gregory goren <grggoren@gmail.com>
2018-01-28 17:28:46 +0200

    -

--- bias_variance_tradeoff_svm/mother_of_all_tables.py
+++ bias_variance_tradeoff_svm/mother_of_all_tables.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     banned = {i: [] for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]}
     # banned[2].append("164")
     # svms = {"svm_model0.1": pickle.load(open("../svm_model", 'rb'))}
-    # competition_data = preprocess.extract_features_by_epoch("../features_asr_modified")
-    competition_data = preprocess.extract_features_by_epoch("../featuresASR_round1_SVM")
+    competition_data = preprocess.extract_features_by_epoch("../features_asr_modified")
+    # competition_data = preprocess.extract_features_by_epoch("../featuresASR_round1_SVM")
     analyze.create_table(competition_data, svms, banned)
     # analyze.score_experiment(competition_data, svms)
