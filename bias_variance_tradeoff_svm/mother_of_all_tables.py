7e80e0fb16047be05c539d86270110daed9046ab
gregory goren <grggoren@gmail.com>
2018-01-29 19:41:05 +0200

    -

--- bias_variance_tradeoff_svm/mother_of_all_tables.py
+++ bias_variance_tradeoff_svm/mother_of_all_tables.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     # C_array.extend([900, 800, 600, 700])
     C_array = []
     # svms = upload_models("models_light", C_array)
-    keys = sorted(svms.keys(), key=lambda x: float(x.split("svm_model")[1]))
+    # keys = sorted(svms.keys(), key=lambda x: float(x.split("svm_model")[1]))
     preprocess = p.preprocess()
     analyze = a.analyze()
 
