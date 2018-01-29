64683899efb2059e1a3b5896d9331d1d5c33e178
gregory goren <grggoren@gmail.com>
2018-01-29 21:59:10 +0200

    -

--- bias_variance_tradeoff_LambdaMart/analyze.py
+++ bias_variance_tradeoff_LambdaMart/analyze.py
@@ -458,15 +458,15 @@ class analyze:
         corr_trees = kendalltau(trees_for_pearson, ndcg_for_pearson)
         corr_leaves = kendalltau(leaves_for_pearson, ndcg_for_pearson)
         f.write(
-            "\\ndcg & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
+            "ndcg & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
                 round(
                     corr_leaves[0], 3)) + " (" + str(round(corr_leaves[1], 3)) + ")   \\\\ \n")
-        # corr_trees = kendalltau(trees_for_pearson, map_for_pearson)
-        # corr_leaves = kendalltau(leaves_for_pearson, map_for_pearson)
-        # f.write(
-        #     "\map & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
-        #         round(
-        #             corr_leaves[0], 3)) + " (" + str(round(corr_leaves[1], 3)) + ")   \\\\ \n")
+        corr_trees = kendalltau(trees_for_pearson, map_for_pearson)
+        corr_leaves = kendalltau(leaves_for_pearson, map_for_pearson)
+        f.write(
+            "map & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
+                round(
+                    corr_leaves[0], 3)) + " (" + str(round(corr_leaves[1], 3)) + ")   \\\\ \n")
         # corr_trees = kendalltau(trees_for_pearson, mrr_for_pearson)
         # corr_leaves = kendalltau(leaves_for_pearson, mrr_for_pearson)
         # f.write(
@@ -669,12 +669,12 @@ class analyze:
             "KTD sum norm & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
                 round(
                     corr_leaves[0], 3)) + " (" + str(round(corr_leaves[1], 3)) + ")   \\\\ \n")
-        # corr_trees = kendalltau(trees_for_pearson, ndcg_for_pearson)
-        # corr_leaves = kendalltau(leaves_for_pearson, ndcg_for_pearson)
-        # f.write(
-        #     "\\ndcg & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
-        #         round(
-        #             corr_leaves[0], 3)) + " (" + str(round(corr_leaves[1], 3)) + ")   \\\\ \n")
+        corr_trees = kendalltau(trees_for_pearson, ndcg_for_pearson)
+        corr_leaves = kendalltau(leaves_for_pearson, ndcg_for_pearson)
+        f.write(
+            "\\ndcg & " + str(round(corr_trees[0], 3)) + " (" + str(round(corr_trees[1], 3)) + ") & " + str(
+                round(
+                    corr_leaves[0], 3)) + " (" + str(round(corr_leaves[1], 3)) + ")   \\\\ \n")
         corr_trees = kendalltau(trees_for_pearson, map_for_pearson)
         corr_leaves = kendalltau(leaves_for_pearson, map_for_pearson)
         f.write(
